23cf1ee7a2ba3dce362a4067a359f3915ea4a64e
Rgonzalez1974 <104175439+Rgonzalez1974@users.noreply.github.com>
2022-06-16 16:40:27 -0300

    new folder

---
+++ app.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+const server = express();
+
+server.listen(2020, () => console.log('Abriendo el servidor http://localhost:2020'));
+
+const public = path.resolve(__dirname, './public');
+
+server.use(express.static(public));
+
+
+
+
+server.get('/', (req, res) => {res.sendFile(path.resolve(__dirname, 'views/home.html'))})
+server.get('/register', (req, res) => {res.sendFile(path.resolve(__dirname, 'views/register.html'))})
+server.get('/login', (req, res) => {res.sendFile(path.resolve(__dirname, 'views/login.html'))})
+server.post('/', (req, res) => {res.sendFile(path.resolve(__dirname, 'views/home.html'))  })
