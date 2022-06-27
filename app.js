a18c8bbdd79a3bec687a6a246369ff622a1b0428
Rgonzalez1974 <104175439+Rgonzalez1974@users.noreply.github.com>
2022-06-27 20:46:21 -0300

    correccion error heroku

--- app.js
+++ app.js
@@ -17,4 +17,4 @@ server.use(express.static(public));
 server.get('/', (req, res) => {res.sendFile(path.resolve(__dirname, 'views/home.html'))})
 server.get('/register', (req, res) => {res.sendFile(path.resolve(__dirname, 'views/register.html'))})
 server.get('/login', (req, res) => {res.sendFile(path.resolve(__dirname, 'views/login.html'))})
-server.post('/', (req, res) => {res.sendFile(path.resolve(__dirname, 'views/home.html'))  })
+// server.post('/', (req, res) => {res.sendFile(path.resolve(__dirname, 'views/home.html'))  })
